f614e6177eedc10c5ff024eb5eb91e5831f07f21
dhanushrajagopal <74738177+dhanushrajagopal@users.noreply.github.com>
2020-12-16 02:32:01 +0530

    Add files via upload

---
+++ DOM/javascript.js
@@ -0,0 +1,21 @@
+
+var randomNumber1;
+randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImages="./images/dice"+randomNumber1+".png";
+var image=document.querySelector(".img1");
+image.setAttribute("src",randomImages);
+var randomNumber2;
+randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImages1="./images/dice"+randomNumber2+".png";
+var image2=document.querySelector(".img2");
+image2.setAttribute("src",randomImages1);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if(randomNumber2==randomNumber1){
+    document.querySelector("h1").innerHTML="Draw";
+}
+else{
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+}
+
