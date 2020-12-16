c3ca15658972de53a7f571ff761cdf180160ee7b
dhanushrajagopal <74738177+dhanushrajagopal@users.noreply.github.com>
2020-12-16 19:33:20 +0530

    Update javascript.js

--- DOM/javascript.js
+++ DOM/javascript.js
@@ -1,4 +1,8 @@
 var randomNumber1;
+var player1=prompt("Enter 1st player name");
+var player2=prompt("Enter 2nd player name");
+document.querySelector(".player01").innerHTML=player1;
+document.querySelector(".player02").innerHTML=player2;
 randomNumber1=Math.floor(Math.random()*6)+1;
 var randomImages="./images/dice"+randomNumber1+".png";
 var image=document.querySelector(".img1");
@@ -9,12 +13,12 @@ var randomImages1="./images/dice"+randomNumber2+".png";
 var image2=document.querySelector(".img2");
 image2.setAttribute("src",randomImages1);
 if(randomNumber1>randomNumber2){
-    document.querySelector("h2").textContent="Player 1 Wins";
+    document.querySelector("h2").innerHTML=player1+"  Wins";
 }
 else if(randomNumber2==randomNumber1){
-    document.querySelector("h2").textContent="Draw";
+    document.querySelector("h2").innerHTML="Draw";
 }
 else{
-    document.querySelector("h2").textContent="Player 2 Wins";
+    document.querySelector("h2").innerHTML=player2+"  Wins";
 }
 
